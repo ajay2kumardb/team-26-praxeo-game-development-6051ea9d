3e266003b8c72bfd0fd2fb479c959620c2d81839
Kris <kmcbrid@gmail.com>
2023-09-21 15:19:40 +0000

    test map

---
+++ tests/test_map.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from levelup.map import GameMap
+
+class TestMapInitWithPostition(TestCase):
+    def test_init(self):
+        ARBITRARY_POSITIONS = 350
+        testobj = GameMap(ARBITRARY_POSITIONS)
+        self.assertEqual(ARBITRARY_POSITIONS, testobj.numPositions)
