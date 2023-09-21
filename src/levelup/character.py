61ce1897acd67f332b0c853cb54e32c6e046ebf3
Ward <levelupward@victaward.us>
2023-09-21 18:30:00 +0000

    Added getName method

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,13 @@
+from levelup.map import GameMap
+
 class Character:
-    name = ""
+ #   name = ""
+    position = (0,0)
+    map = GameMap
 
     def __init__(self, character_name):
         self.name = character_name
+        print(self.name)
+
+    def getName(self):
+        return self.name
