5b2887d1737d094bfdb03716a6eaf954372480ea
Ajay Kumar <ajay2kumardb@gmail.com>
2023-09-21 18:00:43 +0000

    Added Position min and max

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,3 +1,7 @@
 class Position:
    xCoordinates: int = 0
    yCoordinates: int = 0 
+   xCoordinates_max: int = 9
+   yCoordinates_max: int = 9
+   xCoordinates_min: int = 0
+   yCoordinates_min: int = 0
--- tests/test_position.py
+++ tests/test_position.py
@@ -9,3 +9,17 @@ class TestPosition(TestCase):
         testObj = Position()
         self.assertEqual(xCoordinates, testObj.xCoordinates)
         self.assertEqual(yCoordinates, testObj.yCoordinates)
+
+    def test_maxpos(self):
+        xCoordinates_max = 9
+        yCoordinates_max = 9  
+        testObj = Position()
+        self.assertEqual(xCoordinates_max, testObj.xCoordinates_max)
+        self.assertEqual(yCoordinates_max, testObj.yCoordinates_max)      
+
+    def test_minpos(self):
+        xCoordinates_min = 0
+        yCoordinates_min = 0  
+        testObj = Position()
+        self.assertEqual(xCoordinates_min, testObj.xCoordinates_min)
+        self.assertEqual(yCoordinates_min, testObj.yCoordinates_min)  
