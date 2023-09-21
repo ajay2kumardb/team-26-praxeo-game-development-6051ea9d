8359f77f4a3a0f8968259c002e54e3a4d0d02ef4
Ward <levelupward@victaward.us>
2023-09-21 20:47:44 +0000

    Updated moveCharacter

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -2,11 +2,12 @@ from levelup.map import GameMap
 
 class Character:
  #   name = ""
-    position = (0,0)
-    map = GameMap
+    position: tuple = (3, 4)
+    map = GameMap(100)
 
     def __init__(self, character_name):
         self.name = character_name
+        self.position: tuple = (3, 4)
         print(self.name)
 
     def getName(self):
@@ -14,3 +15,7 @@ class Character:
     
     def enterMap(self, map):
         self.map = GameMap(map)
+
+    def moveDirection(self, direction):
+        self.position = self.map.calculatePosition(self.position, direction)
+        return self.position
