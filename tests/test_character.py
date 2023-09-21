d32838ad3e9a7917dd13eb87e7cdf53450e41c12
Ward <levelupward@victaward.us>
2023-09-21 15:25:23 +0000

    Initial tests

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,8 +1,14 @@
 from unittest import TestCase
 from levelup.character import Character
+from levelup.position import Position
 
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_position(self):
+        position = Position(10,1)
+        character.position = position
+        self.assertEqual(character.position, position)
