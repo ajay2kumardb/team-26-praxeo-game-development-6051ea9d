3669c5dc05d21ec8f3697447b18a5a2fa309a228
Andre Le <andre.le70@gmail.com>
2023-09-21 19:49:27 +0000

    Updated position class

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,7 +1,5 @@
-class Position:
-   xCoordinates: int = 0
-   yCoordinates: int = 0 
-   xCoordinates_max: int = 9
-   yCoordinates_max: int = 9
-   xCoordinates_min: int = 0
-   yCoordinates_min: int = 0
+class Position():
+
+   def __init__(self, xCoordinates, yCoordinates):
+      self.xCoordinates = xCoordinates
+      self.yCoordinates = yCoordinates
--- tests/test_position.py
+++ tests/test_position.py
@@ -3,24 +3,10 @@ from levelup.position import Position
 
 class TestPosition(TestCase):
 
-    def test_init(self):
-        xCoordinates = 0
-        yCoordinates = 0
-        testObj = Position()
-        self.assertEqual(xCoordinates, testObj.xCoordinates)
-        self.assertEqual(yCoordinates, testObj.yCoordinates)
-
-    def test_maxpos(self):
-        xCoordinates_max = 9
-        yCoordinates_max = 9  
-        testObj = Position()
-        self.assertEqual(xCoordinates_max, testObj.xCoordinates_max)
-        self.assertEqual(yCoordinates_max, testObj.yCoordinates_max)      
-
-    def test_minpos(self):
-        xCoordinates_min = 0
-        yCoordinates_min = 0  
-        yCoordinates_min = 0  
-        testObj = Position()
-        self.assertEqual(xCoordinates_min, testObj.xCoordinates_min)
-        self.assertEqual(yCoordinates_min, testObj.yCoordinates_min)  
+     def test_init(self):
+        xCoordinates = 5
+        yCoordinates = 4
+        testObj = Position(xCoordinates, yCoordinates)
+        self.assertIsNotNone(testObj.xCoordinates)
+        self.assertIsNotNone(testObj.yCoordinates)
+        
