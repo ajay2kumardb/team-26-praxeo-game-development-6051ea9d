e49fd5953dd2a6decce16226500c930f6e43fca4
Kris <kmcbrid@gmail.com>
2023-09-21 15:40:39 +0000

    Map

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -2,4 +2,7 @@ class GameMap:
     numPositions = None
 
     def __init__(self, numPositions):
-        self.numPositions = 100  
+        self.numPositions = 100
+
+    def getTotalPositions():
+        return numPositions  
