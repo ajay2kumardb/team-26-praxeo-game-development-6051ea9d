7c7b5809fa450d139f4bf4a372621c027de9cad1
Kris <kmcbrid@gmail.com>
2023-09-22 15:31:59 +0000

    UI changes

--- src/levelup/ui.py
+++ src/levelup/ui.py
@@ -10,6 +10,10 @@ class GameApp:
 
     def __init__(self):
         self.controller = GameController()
+        print("\nWelcome to LevelUp World \n\nEnter a world of danger, gold, magic, and death at every turn.\n\nIn the LevelUp World you will explore magical landscapes, collect riches beyond your wildest dreams, \nand experiance the adventure of a lifetime!")
+        response = input("\nDo you dare enter? Y, N\n")
+        if response != "Y":
+            quit()
 
     def prompt(self, menu: str, validation_fn: Callable[[str], bool]) -> str:
         while True:
